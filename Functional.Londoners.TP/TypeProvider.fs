5e9e9e9bf6fa214c0cff464707ea8227b5e9acd9
mavnn <michael@mavnn.co.uk>
2014-03-01 12:08:04 +0000

    Add minimal type

--- Functional.Londoners.TP/TypeProvider.fs
+++ Functional.Londoners.TP/TypeProvider.fs
@@ -2,10 +2,20 @@
 
 open ProviderImplementation.ProvidedTypes
 open Microsoft.FSharp.Core.CompilerServices
+open System.Reflection
 
 [<TypeProvider>]
-type MyTypeProvider () =
+type MyTypeProvider () as this =
     inherit TypeProviderForNamespaces ()
+
+    let ns = "F#unctional.Londoners.Provided"
+    let asm = Assembly.GetExecutingAssembly()
+
+    let newType = 
+        ProvidedTypeDefinition(asm, ns, "NewType", Some typeof<obj>)
+
+    do
+        this.AddNamespace(ns, [newType])
 
 [<assembly:TypeProviderAssembly>]
 do ()
